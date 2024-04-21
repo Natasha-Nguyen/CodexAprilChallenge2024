c03db44cb67cace8c0151df5719797db5d929cd9
Natasha-Nguyen <natasha-nguyen@bigpond.com>
2024-04-21 10:33:23 +1000

    Formatted code in  Welcome.js and Results.js

--- personality-quiz-app/src/Results.js
+++ personality-quiz-app/src/Results.js
@@ -1,30 +1,45 @@
 export default function Results() {
-    const RestartQuiz = () => {
-        console.log("Quiz Restarted");
-        window.location.reload();
-        window.scrollTo(0,0);
-      };
+  const RestartQuiz = () => {
+    console.log("Quiz Restarted");
+    window.location.reload();
+    window.scrollTo(0, 0);
+  };
 
-      return (
-        <div className="flex flex-col items-center justify-center px-4 sm:px-0">
-          <img src="./fishing-character.png" className="w-full sm:w-auto" alt="Fishing Character" />
-          <div className="border-8 border-white p-10 w-full md:w-1/3 sm:w-full">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl text-sky-600">You Caught...</h1>
-          <br></br>
-<p className="text-sm sm:text-base md:text-lg text-sky-600">You love the great outdoors and spending time by the water. You are patient and enjoy the thrill of the catch. Keep on fishing!</p>
-          </div>
-          <button className="bg-sky-400 hover:bg-sky-600 border-4 border-white text-slate-100 p-5 w-full sm:w-auto mt-10"
-            onClick={RestartQuiz}>
-            Fish Again
-          </button>
-          <div className="grid grid-cols-2 sm:grid-cols-2 gap-10 content-start mt-5">
-            <button>
-              <img className="w-full h-auto" src="https://img.icons8.com/color/96/twitterx--v1.png" alt="share to twitterx" />
-            </button>
-            <button>
-              <img className="w-full h-auto" src="./url.svg" alt="copy url" />
-            </button>
-          </div>
-        </div>
-      )
+  return (
+    <div className="flex flex-col items-center justify-between px-4 sm:px-0">
+      <img
+        src="./fishing-character.png"
+        className="w-full sm:w-auto"
+        alt="Fishing Character"
+      />
+      <div className="border-8 border-white p-10 w-full md:w-1/3 sm:w-full">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl text-sky-600">
+          You Caught...
+        </h1>
+        <br></br>
+        <p className="text-sm sm:text-base md:text-lg text-sky-600">
+          You love the great outdoors and spending time by the water. You are
+          patient and enjoy the thrill of the catch. Keep on fishing!
+        </p>
+      </div>
+      <button
+        className="bg-sky-400 hover:bg-sky-600 border-4 border-white text-slate-100 p-5 w-full sm:w-auto mt-10"
+        onClick={RestartQuiz}
+      >
+        Fish Again
+      </button>
+      <div className="grid grid-cols-2 sm:grid-cols-2 gap-10">
+        <button>
+          <img
+            className="w-full h-auto"
+            src="https://img.icons8.com/color/96/twitterx--v1.png"
+            alt="share to twitterx"
+          />
+        </button>
+        <button>
+          <img className="w-full h-auto" src="./url.svg" alt="copy url" />
+        </button>
+      </div>
+    </div>
+  );
 }
--- personality-quiz-app/src/Welcome.js
+++ personality-quiz-app/src/Welcome.js
@@ -5,18 +5,39 @@ export default function Welcome() {
   };
 
   return (
-    <div className = "header">
-        <div className = "header flex flex-col items-center justify-between h-screen">
-          <div></div>
-          <button
+    <div className="header">
+      <div className="header flex flex-col items-center justify-between h-screen">
+        <div></div>
+        <button
           className="bg-sky-400 hover:bg-sky-600 border-4 border-white p-5 text-white mb-20 text-[24px]"
           onClick={startQuiz}
         >
           Start Fishing
         </button>
-        <p className = "text-slate-100 mb-5">Made by <a href = "https://github.com/Natasha-Nguyen" className = "text-slate-100 underline">@nhicodes</a> and <a href = "https://github.com/kriztin12" className = "text-slate-100 underline">@kwicodes</a> for <a href = "https://www.codedex.io/community/monthly-challenge/april-2024" className = "text-slate-100 underline">Codédex April Monthly Challenge</a></p>
-        </div>
-        
+        <p className="text-xs xs:text-base md:text-sm text-slate-100 mb-5">
+          Made by{" "}
+          <a
+            href="https://github.com/Natasha-Nguyen"
+            className="text-slate-100 underline"
+          >
+            @nhicodes
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/kriztin12"
+            className="text-slate-100 underline"
+          >
+            @kwicodes
+          </a>{" "}
+          for{" "}
+          <a
+            href="https://www.codedex.io/community/monthly-challenge/april-2024"
+            className="text-slate-100 underline"
+          >
+            Codédex April Monthly Challenge
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
