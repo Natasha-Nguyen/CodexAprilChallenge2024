1b7277c4094cdc085a9584caef321fdb9565b4e4
kriztin12 <kriztinmargaret@gmail.com>
2024-05-01 01:34:11 +1000

    updated readme and fix refresh

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# CodexAprilChallenge2024
+# Fish Personality Quiz
+- Made for Codédex April Challenge 2024
+- Made by [@nhicodes](https://github.com/Natasha-Nguyen) and [@kwicodes](https://github.com/kriztin12)
+
+## Description
+Dive into the ocean of personality traits and discover which fish species best represents you. Answer the questions to reveal your aquatic alter ego!
+
+Take the quiz here: [Start Fishing!](https://natasha-nguyen.github.io/CodexAprilChallenge2024/)
+
+
+Please do not copy or redistribute our art without permission!
--- personality-quiz-app/src/App.js
+++ personality-quiz-app/src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
+import React, { useState, useEffect } from 'react';
 import Quiz from "./Quiz.js";
 import Welcome from "./Welcome.js";
-import Results from "./Results.js";
 
 function App() {
-  // This function is called when the user clicks on the "Start" button
-  // Then scrolls to the first question section
-  const startQuiz = () => {
-    console.log("Quiz started");
-    document.querySelector(".first").scrollIntoView({ behavior: "smooth" });
-  };
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
 
   return (
     <div className="App">
